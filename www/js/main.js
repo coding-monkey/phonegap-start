a935760e1dca7fe4040bacdc1193b3e132101178
coding_monkey <coding_money@yahoo.com>
2013-02-20 19:14:51 -0500

    need to use a variable to reference this...

--- www/js/main.js
+++ www/js/main.js
@@ -22,8 +22,9 @@ var app = {
 	},
 	
     initialize: function() {
+    	var self = this;
         this.store = new MemoryStore(function() {
-        	this.showAlert('Store Initialized', 'Info');
+        	self.showAlert('Store Initialized', 'Info');
     	});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
