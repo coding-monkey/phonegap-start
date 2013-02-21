42b03aff482a84cf7955aa2143635bef855f7b77
coding_monkey <coding_money@yahoo.com>
2013-02-20 19:38:21 -0500

    tutorial parts 4,5

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,9 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+                $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
@@ -20,13 +14,19 @@ var app = {
 	        alert(title ? (title + ": " + message) : message);
 	    }
 	},
+
+	renderHomeView: function() {
+	    $('body').html(this.homeTpl());
+	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
 	
     initialize: function() {
     	var self = this;
-        this.store = new MemoryStore(function() {
-        	self.showAlert('Store Initialized', 'Info');
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+		this.store = new MemoryStore(function() {
+        	self.renderHomeView();
     	});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
